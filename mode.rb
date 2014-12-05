8af956fc99b7f521abb7013e0c2e2156a48b97d4
Tyler Kowalewski <tykowale@gmail.com>
2014-12-04 22:22:09 -0600

    Figured out the mode

--- mode.rb
+++ mode.rb
@@ -1,8 +1,9 @@
-arr1 = [1,2,3,3]
+arr1 = [1,2,3]
 
 def mode(arr)
   freq = arr.inject(Hash.new(0)) { |num, count| num[count] += 1; num }
   puts freq
+  freq.each { |k, v| puts k if v == freq.values.max }
 end
 
 mode(arr1)
