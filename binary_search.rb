66352f16157b7e0ffd67d865e6b060418555763c
Tyler Kowalewski <tykowale@gmail.com>
2015-02-25 19:06:46 -0600

    I wanted to do this recursively, this is hacked together in like 10 minutes

---
+++ binary_search.rb
@@ -0,0 +1,32 @@
+
+def binary_search(value, array, counter=0)
+  mid = (array.length / 2.0).ceil
+
+  if array.length > 2
+    split_array = array.each_slice(mid).to_a
+  elsif array.length == 2
+    return counter if array[0] == value
+    return counter + 1 if array[1] == value
+  end
+
+  counter += mid
+
+  if array[mid] == value
+    return counter
+  elsif array[mid] > value
+    binary_search(value, split_array[0], counter - mid)
+  elsif array[mid] < value
+    binary_search(value, split_array[1], counter)
+  else
+    return nil
+  end
+
+
+end
+
+
+
+
+# p binary_search(2, [1,2,3,4,5,6,7,8,9])
+# p binary_search(6, [1,2,3,4,5,6,7,8,9])
+p binary_search(500, (1..1000).to_a)
